0217406ccf221d2aedc9e1333d42007c1c81cfc1
Trystan ROLLAND <trystan.rolland1@gmail.com>
2025-03-06 16:39:25 +0100

    wip services

--- orders-service/app.ts
+++ orders-service/app.ts
@@ -1,41 +1,48 @@
-import express, { Request, Response } from "express";
-import mongoose, { Schema, Document } from "mongoose";
+import express, {Request, Response} from "express";
+import mongoose, {Schema, Document} from "mongoose";
 import OrderModel from "./db/Order";
+import dotenv from "dotenv";
 
 const app = express();
 const port: number = 3000;
 
+dotenv.config();
 app.use(express.json());
 
-const mongoURI = "mongodb://root:pass@orders-db/orders-db?authSource=admin"; // Update with your MongoDB URI
-mongoose.connect(mongoURI);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.get("/orders", async (req: Request, res: Response) => {
-  try {
-    const orders = await OrderModel.find();
-    res.status(200).json(orders);
-  } catch (error) {
-    res.status(500).json({ error: "Error getting orders" });
-  }
+    try {
+        const orders = await OrderModel.find();
+        res.status(200).json(orders);
+    } catch (error) {
+        res.status(500).json({error: "Error getting orders"});
+    }
 });
 
 app.post("/orders", async (req: Request, res: Response) => {
-  try {
-    const newOrder = new OrderModel({
-      commandNumber: Math.random().toString(36).substring(7),
-    });
+    try {
+        const newOrder = new OrderModel({
+            commandNumber: Math.random().toString(36).substring(7),
+        });
 
-    await newOrder.save();
-    res.status(201).json(newOrder);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+        await newOrder.save();
+        res.status(201).json(newOrder);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
 });
 
-const server = app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+const server = app.listen(port, async () => {
+    const mongoURI = process.env.MONGO_URI; // Update with your MongoDB URI
+    console.log(mongoURI);
+    try {
+        await mongoose.connect(mongoURI);
+    } catch (err) {
+        console.error(err);
+    }
+    console.log(`Listening on port ${port}`);
 });
